46b7cd90dc0604c7da0cea82ec938d8621514247
Caius Durling <dev@caius.name>
2020-05-23 14:01:31 +0100

    With no args, copy stdin to stdout

--- cat.ts
+++ cat.ts
@@ -1,10 +1,12 @@
-console.log(Deno.args);
+if (Deno.args.length == 0) {
+  await Deno.copy(Deno.stdin, Deno.stdout);
+  Deno.exit(0);
+}
 
 for (const i in Deno.args) {
   const candidate = Deno.args[i];
 
   if (candidate == "-") {
-    // TODO: read from stdin
     await Deno.copy(Deno.stdin, Deno.stdout);
   } else {
     // File on filesystem!
@@ -12,3 +14,5 @@ for (const i in Deno.args) {
     await Deno.copy(file, Deno.stdout);
   }
 }
+
+Deno.exit(0);
