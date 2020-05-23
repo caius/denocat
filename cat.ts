ed3beff101d6ac8a34ffc6fbe8dcea045231225e
Caius Durling <dev@caius.name>
2020-05-23 13:59:43 +0100

    Read files from args to stdout in order

--- cat.ts
+++ cat.ts
@@ -1,1 +1,13 @@
-console.log("Hello?");
+console.log(Deno.args);
+
+for (const i in Deno.args) {
+  const candidate = Deno.args[i];
+
+  if (candidate == "-") {
+    // TODO: read from stdin
+  } else {
+    // File on filesystem!
+    const file = await Deno.open(candidate);
+    Deno.copy(file, Deno.stdout);
+  }
+}
